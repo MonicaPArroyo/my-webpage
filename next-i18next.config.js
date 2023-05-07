feec94ef9f18c6b196008e6573d7974cc0b68058
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-05-06 19:42:38 -0600

    Configuraciones para cambio de idioma

---
+++ next-i18next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'es'],
+    },
+    debug: process.env.NODE_ENV === 'development',
+    
+
+    /** To avoid issues when deploying to some paas (vercel...) */
+    localePath:
+    typeof window === 'undefined'
+    ? require('path').resolve('./public/locales')
+    : '/locales',
+
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
+}
